12f367af21f86e314c6dc01edc4278b9357ea79a
Alma Perez <aperez32@student.scf.edu>
2022-07-19 11:17:04 -0400

    Arithmetic lab DONE

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
-let num1 =31 
-let num2 =2 
-let multiply = num1 * num2 
-let random = Math.floor(Math.random() * 10) + 1;
-let num3 =20
-let num4= 5
-let mod = 20/5
-let max = (1, 2, 3, 4, 20);
+// let num1 =31 
+// let num2 =2 
+// let multiply = num1 * num2 
+// let random = Math.floor(Math.random() * 10) + 1;
+// let num3 =20
+// let num4= 5
+// let mod = 20/5
+// let max = (1, 2, 3, 4, 20);
+
+ let num1= 1
+ let num2= 62
+ let multiply = num1 * num2 
+ let random = Math.floor(Math.random() * 10) + 1;
+ let num3=12
+ let num4=3
+ let mod = num3/num4 
+ let max = (5,10,15,20);
